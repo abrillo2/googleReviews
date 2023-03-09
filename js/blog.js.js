a0f77bab481a56b0a364a193d00dc08b4fc8062e
abrillo2 <it.abreham.ararsa@gmail.com>
2023-03-09 21:59:41 +0300

    Delete blog.js.js

--- js/blog.js.js
+++
@@ -1,8 +0,0 @@
-function httpGet(theUrl){
-                    var xmlHttp = new XMLHttpRequest();
-                    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-                    xmlHttp.send( null );
-                    return xmlHttp.responseText;
-}
-const response = httpGet("https://script.google.com/macros/s/AKfycbztfObrkDrGDgZeVxxcmCzmb-k-3mjYzxqT4UxLSNQOOK7osIEN0jM58h5sKSxAkIkQnQ/exec");
-console.log(JSON.stringify(response));
